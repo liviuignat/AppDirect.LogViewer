79f36bb276d773e987e7ff36e9b80a8ab771ec1f
aardelean <alex.ardelean@appdirect.com>
2016-07-26 11:43:49 +0200

    added load more button

--- src/main/home/pages/HomePage/HomePage.js
+++ src/main/home/pages/HomePage/HomePage.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 import {asyncConnect} from 'redux-async-connect';
 import {Paper} from 'main/common/components';
 import {LogList} from 'main/home/components';
-import {loadLogsAction, toggleLogDetailDialog} from 'main/home/actions';
+import {loadLogsAction, toggleLogDetailDialog, viewMoreLogsAction} from 'main/home/actions';
 
 @asyncConnect([{
   promise: ({store}) => {
@@ -18,35 +18,42 @@ import {loadLogsAction, toggleLogDetailDialog} from 'main/home/actions';
 @connect(
   ({home}) => ({
     logs: home.logs,
+    query: home.query,
     isDetailDialogOpen: home.isDetailDialogOpen,
     selectedLog: home.selectedLog,
-  }), {loadLogsAction, toggleLogDetailDialog})
+  }), {loadLogsAction, toggleLogDetailDialog, viewMoreLogsAction})
 export default class HomePage extends Component {
   static propTypes = {
     logs: PropTypes.array.isRequired,
+    query: PropTypes.object.isRequired,
     isDetailDialogOpen: PropTypes.bool.isRequired,
     selectedLog: PropTypes.object.isRequired,
     toggleLogDetailDialog: PropTypes.func.isRequired,
-    loadLogsAction: PropTypes.func.isRequired
+    loadLogsAction: PropTypes.func.isRequired,
+    viewMoreLogsAction: PropTypes.func.isRequired
   }
 
   componentDidMount() {
-    window.setInterval(() => this.props.loadLogsAction(), 5000);
+    window.setInterval(() => {
+      this.props.loadLogsAction({size: this.props.query.size});
+    }, 5000);
   }
 
   render() {
     const css = require('./HomePage.scss');
-    const {logs, selectedLog, isDetailDialogOpen} = this.props;
-
+    const {logs, selectedLog, isDetailDialogOpen, query} = this.props;
     return (
       <Paper className={css.HomePage}>
         <Helmet title="AppDirect - Logs" />
 
         <LogList
           logs={logs}
+          query={query}
           isDetailDialogOpen={isDetailDialogOpen}
           selectedLog={selectedLog}
-          toggleLogDetailDialog={this.props.toggleLogDetailDialog} />
+          toggleLogDetailDialog={this.props.toggleLogDetailDialog}
+          loadLogsAction={this.props.loadLogsAction}
+          viewMoreLogsAction={this.props.viewMoreLogsAction}/>
       </Paper>
     );
   }
